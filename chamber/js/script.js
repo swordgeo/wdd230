e4a61a52ed8bd4d722c03e5abf710638e08fc8f5
swordgeo <swordgeo1094@yahoo.com>
2022-01-30 13:33:30 -0500

    Week 4 finished

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,40 +1,25 @@
+
+//hamburger
+function toggleMenu() {
+  document.getElementById('primaryNav').classList.toggle("open");
+  document.getElementById('hamburgerBtn').classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
+//end hamburger
+
 const yearDisplay = document.querySelector('footer h2');
 var today = new Date();
 var todayYear = today.getFullYear();
 yearDisplay.innerHTML = `&copy; ${todayYear} - George Blanchard - Indiana`;
 
-const modDateDisplay = document.querySelector('footer p');
+const modDateDisplay = document.getElementById('modDate');
 let modDate = document.lastModified;
 modDateDisplay.textContent = ("Last modified "+modDate);
-
-
-//Hamburger menu stuff
-//If problems refer to https://codepen.io/blazzard-jason/pen/gmGxaj/
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-//End hamburger
-
-
-//Date stuff
 
 // select the elements to manipulate (output to)
 const datefield = document.querySelector("date");
 const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
-
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-// long, medium, short options ... try them
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(today);
+datefield.innerHTML = `<strong>${fulldate}</strong>`;
