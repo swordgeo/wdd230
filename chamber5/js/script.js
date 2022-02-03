d6d01c52f083f4bacb3ccf5ea1a9978df32ed5dc
swordgeo <swordgeo1094@yahoo.com>
2022-02-03 13:08:20 -0500

    Week 5 minus beautification

--- chamber5/js/script.js
+++ chamber5/js/script.js
@@ -7,18 +7,23 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
-//end hamburger
 
+
+//dates
 const yearDisplay = document.querySelector('footer h2');
 var today = new Date();
 var todayYear = today.getFullYear();
+//copyright
 yearDisplay.innerHTML = `&copy; ${todayYear} - George Blanchard - Indiana`;
-
+//modDate
 const modDateDisplay = document.getElementById('modDate');
 let modDate = document.lastModified;
 modDateDisplay.textContent = ("Last modified "+modDate);
-
-// select the elements to manipulate (output to)
+//currentDay
 const datefield = document.getElementById('date');
 const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(today);
 datefield.innerHTML = `<strong>${fulldate}</strong>`;
+//weekday banner calculator
+let weekday = today.getDay();
+const banner = document.getElementById('banner');
+if (weekday >=1 && weekday <=2) {banner.style.display = "block";}
