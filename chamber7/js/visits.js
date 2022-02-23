a21bf34eb9b7f607cacb0f2d47e9a12a6a0836fb
swordgeo <swordgeo1094@yahoo.com>
2022-02-23 18:19:04 -0500

    Table Build

--- chamber7/js/visits.js
+++ chamber7/js/visits.js
@@ -6,15 +6,18 @@ rightNow = Number(Date.now());
 
 //Find last visit from Local Storage
 let lastTime = Number(window.localStorage.getItem("last-time"));
+let daysNumber = Math.round((rightNow - lastTime)/86400000);
 
 //Calculate number of days
 //Change the factor from 1000 to 86,400,000 before publish
 if (lastTime !== 0) {
-  daysNumber = Math.round((rightNow - lastTime)/86400000);
-  visitDisplay.textContent = `It's been ${daysNumber} days since your last visit. Welcome Back!`;
+  visitDisplay.innerHTML = `<strong>It's been ${daysNumber} days since your last visit. Welcome Back!</strong>`;
+}
+else if (daysNumber === 1) {
+  visitDisplay.innerHTML = `<strong>It's been ${daysNumber} days since your last visit. Welcome Back!</strong>`;
 }
 else {
-  visitDisplay.textContent = `This is your first time here! Welcome!`;
+  visitDisplay.textContent = `<strong>This is your first time here! Welcome!</strong>`;
 }
 
 localStorage.setItem("last-time", rightNow);
