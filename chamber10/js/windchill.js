3a8dbca1d164298267380de8a4450395dbb42bf3
swordgeo <swordgeo1094@yahoo.com>
2022-03-12 15:41:42 -0500

    Leedle

--- chamber10/js/windchill.js
+++ chamber10/js/windchill.js
@@ -5,21 +5,20 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
 
-    const currentTemp = document.querySelector('#temp');
+    const tempQ = document.querySelector('#temp');
     const weathericon = document.querySelector('#weathericon');
-    const wspeed = document.querySelector('#wspeed');
+    const speedQ = document.querySelector('#wspeed');
 
-    currentTemp.textContent = jsObject.main.temp.toFixed(0);
-    wspeed.textContent = jsObject.wind.speed;
+    let temp = jsObject.main.temp;
+    let speed = jsObject.wind.speed;
+    
+    tempQ.textContent = temp.toFixed(1);
+    speedQ.textContent = speed.toFixed(1);
 
     let imgsrc = `https://api.openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     let imgalt = jsObject.weather[0].description;
     weathericon.setAttribute('src', imgsrc);
     weathericon.setAttribute('alt', imgalt);
-
-    let temp = parseFloat(document.querySelector("#temp").textContent);
-    let speed = parseFloat(document.querySelector("#wspeed").textContent);
-    
     let windchill = "";
     
     //less than 50F; more than 3mph
@@ -35,7 +34,9 @@ fetch(apiURL)
     function windChill(temp, speed) {
       let answer = (35.74 + 0.6215*temp - 35.75*Math.pow(speed,0.16) + 0.4275*temp*Math.pow(speed,0.16));
       //round two decimals
-      return Math.round((answer + Number.EPSILON) * 100) / 100;;
+      return Math.round((answer + Number.EPSILON) * 10) / 10;
     }
 
   });
+
+  //Check Week 10 minute 30:00 if I need to list the weather description in text
