eb83748d1183fe78c08b7d593f717cf8c441696b
swordgeo <swordgeo1094@yahoo.com>
2022-03-14 19:09:03 -0400

    Random

--- chamber11/js/spotlights.js
+++ chamber11/js/spotlights.js
@@ -11,22 +11,54 @@ fetch(requestURL).then(function (response) {return response.json();})
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const businesses = jsonObject['businesses'];
     businesses.forEach(shuffleIn);
+    pushBusiness(random(golden, silver), 1)
+    pushBusiness(random(golden, silver), 2)
+    pushBusiness(random(golden, silver), 3);
   });
 
 function shuffleIn(business) {
-  if (business.membership = "gold") {
+  if (business.membership == "gold") {
     golden.push(business);
   }
-  else if (business.membership = "silver") {
+  else if (business.membership == "silver") {
     silver.push(business);
   }
   else {
 
   }
+}
+
+function random(golden, silver) {
+  if (Math.random() < .66) {
+    let num = Math.floor(Math.random()*golden.length)
+    let pick = golden[num];
+    golden.splice(num, 1);
+    console.log(pick);
+    return pick;
+  }
+  else {
+    let num = Math.floor(Math.random()*silver.length)
+    let pick = silver[num];
+    silver.splice(num, 1);
+    console.log(pick);
+    return pick;
+  }
+}
+
+
+function pushBusiness(subject, spotNumber) {
+
+  document.querySelector(`#spotlight${spotNumber} h3`).textContent = subject.name;
+  document.querySelector(`#spotlight${spotNumber} p`).textContent = subject.message;
+  document.querySelector(`#spotlight${spotNumber} a`).textContent = `(${subject.phone.substring(0,3)}) ${subject.phone.substring(3,6)}-${subject.phone.substring(6,10)}`;
+  document.querySelector(`#spotlight${spotNumber} a`).href = `tel:${subject.phone}`;
+//website
+  document.querySelector(`#spotlight${spotNumber} :nth-child(5)`).textContent = subject.website;
+  document.querySelector(`#spotlight${spotNumber} :nth-child(5)`).href = subject.website;
+  
   
 
 }
-
 
 
 
