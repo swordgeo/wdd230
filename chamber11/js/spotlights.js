ff5e933ede8074a69497cdab9b41dd7b0355f70e
swordgeo <swordgeo1094@yahoo.com>
2022-03-15 15:36:22 -0400

    index fixes

--- chamber11/js/spotlights.js
+++ chamber11/js/spotlights.js
@@ -3,12 +3,12 @@ const requestURL = 'data/data.json';
 const golden = [];
 const silver = [];
 
-console.log(golden);
-console.log(silver);
+//console.log(golden);
+//console.log(silver);
 
 fetch(requestURL).then(function (response) {return response.json();})
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
     const businesses = jsonObject['businesses'];
     businesses.forEach(shuffleIn);
     pushBusiness(random(golden, silver), 1)
@@ -23,9 +23,6 @@ function shuffleIn(business) {
   else if (business.membership == "silver") {
     silver.push(business);
   }
-  else {
-
-  }
 }
 
 function random(golden, silver) {
@@ -33,21 +30,42 @@ function random(golden, silver) {
     let num = Math.floor(Math.random()*golden.length)
     let pick = golden[num];
     golden.splice(num, 1);
-    console.log(pick);
+    //console.log(pick);
     return pick;
   }
   else {
     let num = Math.floor(Math.random()*silver.length)
     let pick = silver[num];
     silver.splice(num, 1);
-    console.log(pick);
+    //console.log(pick);
     return pick;
   }
 }
 
-
 function pushBusiness(subject, spotNumber) {
+  let subName = document.createElement('h3');
+  let subMessage = document.createElement('p');
+  let subPhone = document.createElement('a');
+  let subSite = document.createElement('a');
+  let linebreak = document.createElement('br');
 
+  subName.textContent = subject.name;
+  subMessage.textContent = subject.message;
+  subPhone.textContent = `(${subject.phone.substring(0,3)}) ${subject.phone.substring(3,6)}-${subject.phone.substring(6,10)}`;
+  subPhone.href = `tel:${subject.phone}`;
+  subSite.textContent = subject.website;
+  subSite.href = subject.website;
+  
+  document.querySelector(`#spotlight${spotNumber}`).appendChild(subName);
+  document.querySelector(`#spotlight${spotNumber}`).appendChild(linebreak);
+  document.querySelector(`#spotlight${spotNumber}`).appendChild(subPhone);
+  document.querySelector(`#spotlight${spotNumber}`).appendChild(subMessage);
+  document.querySelector(`#spotlight${spotNumber}`).appendChild(subSite);
+
+}
+
+/*
+function pushBusiness(subject, spotNumber) {
   document.querySelector(`#spotlight${spotNumber} h3`).textContent = subject.name;
   document.querySelector(`#spotlight${spotNumber} p`).textContent = subject.message;
   document.querySelector(`#spotlight${spotNumber} a`).textContent = `(${subject.phone.substring(0,3)}) ${subject.phone.substring(3,6)}-${subject.phone.substring(6,10)}`;
@@ -55,33 +73,5 @@ function pushBusiness(subject, spotNumber) {
 //website
   document.querySelector(`#spotlight${spotNumber} :nth-child(5)`).textContent = subject.website;
   document.querySelector(`#spotlight${spotNumber} :nth-child(5)`).href = subject.website;
-  
-  
-
 }
-
-
-
-/*
-  function displayListings(business) {
-    let listing = document.createElement('li');
-    let name = document.createElement('span');
-    let address = document.createElement('span');
-    let phone = document.createElement('a');
-    let link = document.createElement('a');
-  
-    name.textContent = `${business.name}`;
-    address.textContent = `${business.address}`;
-    phone.href = `tel:${business.phone}`;
-    phone.textContent = `(${business.phone.substring(0,3)}) ${business.phone.substring(3,6)}-${business.phone.substring(6,10)}`;
-    link.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    link.target = "_blank";
-    link.textContent = `${business.website}`;
-  
-    listing.appendChild(name);
-    listing.appendChild(phone);
-    listing.appendChild(address);
-    listing.appendChild(link);
-    document.querySelector('.listings').appendChild(listing);
-  }
-  */
+*/
