14f5b119baa6a10f238cda86dd5036cbb780fc51
swordgeo <swordgeo1094@yahoo.com>
2022-04-02 11:26:22 -0400

    ll

--- final/js/burgers.js
+++ final/js/burgers.js
@@ -5,3 +5,14 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
+
+//
+const yearDisplay = document.querySelector('footer h2');
+var today = new Date();
+var todayYear = today.getFullYear();
+//copyright
+yearDisplay.innerHTML = `&copy; ${todayYear} - George Blanchard - Indiana`;
+//modDate
+const modDateDisplay = document.getElementById('modDate');
+let modDate = document.lastModified;
+modDateDisplay.textContent = ("Last modified "+modDate);
